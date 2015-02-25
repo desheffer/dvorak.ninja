48d384c61d9f8ecc7724872c7791a6862a35689d
Doug Sheffer <doug.sheffer@researchsquare.com>
2015-02-25 01:22:07 -0500

    Add timer countdown, add new Dvorak paragraphs, and improve character mapping

--- app.js
+++ app.js
@@ -3,11 +3,49 @@
         return new Date().getTime() / 1000;
     }
 
-    var view = (function() {
-        var that = {
-            render: function(correctlyTyped, incorrectlyTyped, notYetTyped, seconds) {
-                var completed = (notYetTyped.length == 0);
+    function fromCharCode(keyCode, shiftKey) {
+        var layers = {};
 
+        layers.normal = {
+            32: ' ',
+            48: '0', 49: '1', 50: '2', 51: '3', 52: '4',
+            53: '5', 54: '6', 55: '7', 56: '8', 57: '9',
+            65: 'a', 66: 'b', 67: 'c', 68: 'd', 69: 'e',
+            70: 'f', 71: 'g', 72: 'h', 73: 'i', 74: 'j',
+            75: 'k', 76: 'l', 77: 'm', 78: 'n', 79: 'o',
+            80: 'p', 81: 'q', 82: 'r', 83: 's', 84: 't',
+            85: 'u', 86: 'v', 87: 'w', 88: 'x', 89: 'y',
+            90: 'z',
+            186: ';', 187: '=', 188: ',', 189: '-', 190: '.',
+            191: '/', 192: '`', 219: '[', 220: '\\', 221: ']',
+            222: '\'',
+        };
+
+        layers.shift = {
+            32: ' ',
+            48: ')', 49: '!', 50: '@', 51: '#', 52: '$',
+            53: '%', 54: '^', 55: '&', 56: '*', 57: '(',
+            65: 'A', 66: 'B', 67: 'C', 68: 'D', 69: 'E',
+            70: 'F', 71: 'G', 72: 'H', 73: 'I', 74: 'J',
+            75: 'K', 76: 'L', 77: 'M', 78: 'N', 79: 'O',
+            80: 'P', 81: 'Q', 82: 'R', 83: 'S', 84: 'T',
+            85: 'U', 86: 'V', 87: 'W', 88: 'X', 89: 'Y',
+            90: 'Z',
+            186: ':', 187: '+', 188: '<', 189: '_', 190: '>',
+            191: '?', 192: '~', 219: '{', 220: '|', 221: '}',
+            222: '"',
+        };
+
+        if (shiftKey) {
+            return layers.shift[keyCode];
+        } else {
+            return layers.normal[keyCode];
+        }
+    }
+
+    var view = (function() {
+        return {
+            render: function(started, completed, correctlyTyped, incorrectlyTyped, notYetTyped, seconds) {
                 if (completed) {
                     $('#type').html(
                         '<span class="correct">' + correctlyTyped + '</span>'
@@ -31,29 +69,33 @@
                 $('#stats .seconds .value').text(seconds);
             },
         };
-
-        return that;
     })();
 
     var controller = (function(view) {
-        var wordsToType;
-        var correctlyTyped;
-        var incorrectlyTyped;
-        var notYetTyped;
-        var startTime;
+        var wordsToType = '';
+        var correctlyTyped = '';
+        var incorrectlyTyped = '';
+        var notYetTyped = '';
+        var startTime = null;
+
+        function started() {
+            return wordsToType.length > 0;
+        }
+
+        function completed() {
+            return started() && notYetTyped.length == 0;
+        }
 
         function triggerRender() {
             var seconds = Math.round(time() - startTime);
 
-            view.render(correctlyTyped, incorrectlyTyped, notYetTyped, seconds);
+            view.render(started(), completed(), correctlyTyped, incorrectlyTyped, notYetTyped, seconds);
         }
 
-        var that = {
-            init: function(words) {
+        return {
+            start: function(words) {
                 wordsToType = words;
-                that.reset();
-            },
-            reset: function() {
+
                 correctlyTyped = '';
                 incorrectlyTyped = '';
                 notYetTyped = wordsToType;
@@ -61,11 +103,8 @@
 
                 triggerRender();
             },
-            completed: function() {
-                return notYetTyped.length == 0;
-            },
             letterTyped: function(letter) {
-                if (that.completed()) {
+                if (!started() || completed()) {
                     return;
                 }
 
@@ -81,7 +120,7 @@
                 triggerRender();
             },
             backspaceTyped: function() {
-                if (that.completed()) {
+                if (!started() || completed()) {
                     return;
                 }
 
@@ -95,29 +134,43 @@
                 triggerRender();
             },
         };
-
-        return that;
     })(view);
 
     $(document).on('keydown', function(e) {
-        if (e.keyCode == 17 || e.keyCode == 18 || e.keyCode == 91) {
-            // Ctrl, Alt, Command
+        // Ignore keyboard shortcuts
+        if (e.altKey || e.ctrlKey || e.metaKey) {
             return true;
         }
 
+        // Backspace
         if (e.keyCode == 8) {
-            // Backspace
             controller.backspaceTyped();
             return false;
         }
 
-        var letter = String.fromCharCode(e.keyCode);
-        letter = e.shiftKey ? letter.toUpperCase() : letter.toLowerCase();
-        controller.letterTyped(letter);
+        // Normal keys
+        var letter = fromCharCode(e.keyCode, e.shiftKey);
+        if (letter) {
+            controller.letterTyped(letter);
+        }
     });
 
     $('#paragraphs a').on('click', function() {
-        controller.init($(this).data('paragraph'));
+        var link = $(this);
+
+        var timeout = 3;
+        function countdown() {
+            if (timeout > 0) {
+                $('#type').html('<div class="countdown">' + timeout + '</div>');
+                timeout--;
+                setTimeout(countdown, 1000);
+            } else {
+                controller.start(link.data('paragraph'));
+            }
+        }
+        countdown();
+
+        $(this).blur();
         return false;
     });
 })();

698c83b20db9e40bf1305eacc8496e051513d279
Doug Sheffer <doug.sheffer@researchsquare.com>
2015-03-14 18:52:27 -0400

    Run grunt

--- dist/wpm.js
+++ dist/wpm.js
@@ -483,15 +483,46 @@
 
     window.WPM = window.WPM || {};
 
-    window.WPM.SocialBox = function() {
+    window.WPM.SocialBox = function(social) {
         var firebase = new Firebase(window.WPM.firebaseURL);
         var user;
 
-        firebase.authAnonymously(function(error, authData) {
-            if (!error) {
-                user = authData;
-                console.log('user uid:', user.uid);
+        function djb2(str){
+            var hash = 5381;
+            for (var i = 0; i < str.length; i++) {
+                hash = ((hash << 5) + hash) + str.charCodeAt(i); /* hash * 33 + c */
             }
+            return hash;
+        }
+
+        function userToColor(str) {
+            var hash = djb2(str);
+            var r = (hash & 0xFF0000) >> 16;
+            var g = (hash & 0x00FF00) >> 8;
+            var b = hash & 0x0000FF;
+            return '#' + ('0' + r.toString(16)).substr(-2) + ('0' + g.toString(16)).substr(-2) + ('0' + b.toString(16)).substr(-2);
+        }
+
+        function userToName(str) {
+            return 'Guest' + djb2(str).toString(10).substr(-5);
+        }
+
+        firebase.authAnonymously(function(error, authData) {
+            if (authData) {
+                user = authData;
+
+                var userRef = firebase.child('presence').child(user.uid);
+                userRef.onDisconnect().remove();
+                userRef.set(true);
+
+                social.find('.me .avatar').css('background-color', userToColor(user.uid));
+                social.find('.me .name').text(userToName(user.uid));
+            }
+        });
+
+        firebase.child('presence').on('value', function(snapshot) {
+            var count = Object.keys(snapshot.val()).length;
+            social.find('.user-count .value').text(count);
         });
 
         this.scoreChanged = function(e) {
@@ -499,7 +530,7 @@
                 return;
             }
 
-            firebase.child(user.uid).push({
+            firebase.child('score').child(user.uid).push({
                 timeStamp: e.timeStamp,
                 paragraphName: e.paragraphName,
                 score: {
@@ -542,7 +573,7 @@
             if (e.seconds !== undefined) {
                 var min = ~~(e.seconds / 60);
                 var sec = ~~(e.seconds - min * 60);
-                time = min + ':' + (sec < 10 ? '0' + sec : sec);
+                time = min + ':' + ('0' + sec).substr(-2);
             }
 
             stats.find('.wpm .value').text(~~e.wpm);
@@ -666,17 +697,17 @@
     });
 
     // Views
-    var paraBox = new WPM.ParaBox(WPM.paragraphs, $('#paragraphs'));
+    var paraBox = new WPM.ParaBox(WPM.paragraphs, $('#para-box'));
     $(paraBox).on('paragraphchange.wpm', function(e) {
         game.start(e.name, e.paragraph, 3);
     });
 
-    var typeBox = new WPM.TypeBox($('#type'));
+    var typeBox = new WPM.TypeBox($('#type-box'));
     $(game).on('modechange.wpm', typeBox.modeChanged);
     $(game).on('countdown.wpm', typeBox.countdown);
     $(game).on('textchange.wpm', typeBox.textChanged);
 
-    var statsBox = new WPM.StatsBox($('#stats'));
+    var statsBox = new WPM.StatsBox($('#stats-box'));
     $(game).on('modechange.wpm', statsBox.modeChanged);
     $(game).on('scorechange.wpm', statsBox.scoreChanged);
 
@@ -692,7 +723,7 @@
     $(game).on('textchange.wpm', scoreCard.textChanged);
     $(game).on('scorechange.wpm', scoreCard.scoreChanged);
 
-    var socialBox = new WPM.SocialBox();
+    var socialBox = new WPM.SocialBox($('#social-box'));
     $(game).on('scorechange.wpm', socialBox.scoreChanged);
 
     // Start the game loop

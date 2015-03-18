07ab2729f09747e4379facc586bdf79397145f8b
Doug Sheffer <doug.sheffer@researchsquare.com>
2015-03-18 01:39:48 -0400

    Run grunt

--- dist/wpm.js
+++ dist/wpm.js
@@ -1,4 +1,4 @@
-/*! wpm 2015-03-17 */
+/*! wpm 2015-03-18 */
 (function($) {
     'use strict';
 
@@ -422,15 +422,15 @@
             return hash;
         }
 
-        function userToColor(str) {
-            var hash = djb2(str);
-            var r = (hash & 0xFF0000) >> 16;
-            var g = (hash & 0x00FF00) >> 8;
-            var b = hash & 0x0000FF;
-            return '#' + ('0' + r.toString(16)).substr(-2) + ('0' + g.toString(16)).substr(-2) + ('0' + b.toString(16)).substr(-2);
-        }
+        // function hashToColor(str) {
+        //     var hash = djb2(str);
+        //     var r = (hash & 0xFF0000) >> 16;
+        //     var g = (hash & 0x00FF00) >> 8;
+        //     var b = hash & 0x0000FF;
+        //     return '#' + ('0' + r.toString(16)).substr(-2) + ('0' + g.toString(16)).substr(-2) + ('0' + b.toString(16)).substr(-2);
+        // }
 
-        function userToName(str) {
+        function hashToName(str) {
             return 'Guest' + djb2(str).toString(10).substr(-5);
         }
 
@@ -442,27 +442,65 @@
                 userRef.onDisconnect().remove();
                 userRef.set(true);
 
-                social.find('.me .avatar').css('background-color', userToColor(user.uid));
-                social.find('.me .name').text(userToName(user.uid));
+                // social.find('.me .avatar').css('background-color', hashToColor(user.uid));
+                // social.find('.me .name').text(hashToName(user.uid));
             }
         });
 
-        firebase.child('presence').on('value', function(snapshot) {
-            var count = Object.keys(snapshot.val()).length;
-            social.find('.user-count .value').text(count);
-        });
+        firebase.child('presence')
+            .on('value', function(snapshot) {
+                var count = Object.keys(snapshot.val()).length;
+                social.find('.user-count .value').text(count);
+            });
+
+        firebase.child('score')
+            .orderByChild('timestamp')
+            .limitToLast(5)
+            .on('child_added', function(snapshot) {
+                var table = social.find('table');
+                var tbody = table.find('tbody');
+                if (tbody.length === 0) {
+                    tbody = $('<tbody>').appendTo(table);
+                }
+
+                var val = snapshot.val();
+
+                var time;
+                var min = ~~(val.score.seconds / 60);
+                var sec = ~~(val.score.seconds - min * 60);
+                time = min + ':' + ('0' + sec).substr(-2);
+
+                var date = new Date(val.timestamp);
+
+                var tr = $('<tr>');
+                $('<td>').text(val.user.name).appendTo(tr);
+                $('<td>').text(~~val.score.wpm).appendTo(tr);
+                $('<td>').text(~~val.score.accuracy + '%').appendTo(tr);
+                $('<td>').text(~~val.score.characters).appendTo(tr);
+                $('<td>').text(time).appendTo(tr);
+                $('<td>').text(val.paragraph.name).appendTo(tr);
+                $('<td>').text(date.toLocaleString()).appendTo(tr);
+                tr.prependTo(tbody);
+
+                tbody.slice(5).remove();
+            });
 
         this.scoreChanged = function(e) {
             if (e.complete !== true) {
                 return;
             }
 
-            firebase.child('score').child(user.uid).push({
-                timeStamp: e.timeStamp,
-                paragraphName: e.paragraphName,
+            firebase.child('score').push({
+                user: {
+                    name: hashToName(user.uid),
+                },
+                timestamp: e.timeStamp,
+                paragraph: {
+                    name: e.paragraphName,
+                },
                 score: {
                     seconds: e.seconds,
-                    words: e.words,
+                    characters: e.characters,
                     wpm: e.wpm,
                     accuracy: e.accuracy,
                 },

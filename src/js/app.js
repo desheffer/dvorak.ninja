44986ea6879dd6fead9355d6d71d6eff6a0cb88d
Doug Sheffer <doug.sheffer@researchsquare.com>
2016-05-22 12:16:18 -0400

    Add ga event

--- src/js/app.js
+++ src/js/app.js
@@ -20,6 +20,7 @@
     var wordSetBox = new WPM.WordSetBox(WPM.wordSets, $('#word-set-box'));
     $(wordSetBox).on('wordlistchange.wpm', function(e) {
         game.changeWordList(e.name, e.wordList);
+        ga('send', 'event', 'WordList', 'change', e.name);
     });
 
     var typeBox = new WPM.TypeBox($('#type-box'), $('#overlay-box'));
